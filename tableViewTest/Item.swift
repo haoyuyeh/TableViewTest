c9f889e54d7edc5c684e1d7c44d42ddf8128f89b
haoyuyeh <haoyuyeh@gmail.com>
2017-12-03 01:40:23 +1100

    load item into table view cell

---
+++ tableViewTest/Item.swift
@@ -0,0 +1,26 @@
+//
+//  Item.swift
+//  tableViewTest
+//
+//  Created by Hao Yu Yeh on 2017/12/3.
+//  Copyright © 2017年 Hao Yu Yeh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    var name = ""
+    var image = UIImage(named: "cat.png")
+    var quantity = 0
+    var price = 0.0
+    var minPrice = 0.0
+    var enableExpiredDay = false
+    var purchasedDate = Date(timeIntervalSinceReferenceDate: 1)
+    var expiredDate = Date(timeIntervalSinceReferenceDate: 1)
+    init(name:String, quantity:Int, price: Double) {
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+    }
+}
--- tableViewTest/ViewController.swift
+++ tableViewTest/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var items = [Item]()
+    
+    // MARK : View Lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        items.append(Item(name: "juice", quantity: 2, price: 1.5))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK : Table View Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell")
+        let item = items[indexPath.row]
+        cell?.textLabel?.text = item.name
+        cell?.detailTextLabel?.text = "Quantity:" + String(item.quantity) + ", Price:" + String(item.price)
+        cell?.imageView?.image = item.image
+        return cell!
+    }
 }
 
